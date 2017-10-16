985160da8e8c4b1862f87ecc419d0460df062d54
chenzh <120715979@qq.com>
2017-10-16 10:07:13 +0800

    第四章

--- src/com/czh/stream/Stream4_4_2.java
+++ src/com/czh/stream/Stream4_4_2.java
@@ -5,15 +5,11 @@ import com.czh.publish.Apple;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 /**
- * yellow只会被filter不会被map
- * black不会被filter和map
+ * 实验使用Apple代替Dish
  */
-public class Stream4_4 {
+public class Stream4_4_2 {
     public static void main(String[] args) {
         BiFunction<Integer, String, Apple> bf = Apple::new;
         List<Apple> list = new ArrayList<>();
@@ -22,13 +18,9 @@ public class Stream4_4 {
         list.add(bf.apply(100, "yellow"));
         list.add(bf.apply(200, "blue"));
         list.add(bf.apply(200, "black"));
-        List<String> colors = list.stream().filter((a) ->{
-            System.out.println("filter = "+a.getColor());
-            return a.getWeight() > 150;
-        }).map((a)->{
-            System.out.println("map = " + a.getColor());
-            return a.getColor();
-        }).limit(3).collect(toList());
 
+        list.stream().forEach(apple -> System.out.println(apple));
+        System.out.println("----------------");
+        list.stream().forEach(System.out::println);
     }
 }
