21a3dc5e0832d996ee445ea0b29de28dc5ec8172
chenzh <120715979@qq.com>
2017-10-23 17:48:22 +0800

    初次了解收集器

--- src/com/czh/publish/Test.java
+++ src/com/czh/publish/Test.java
@@ -1,5 +1,10 @@
 package com.czh.publish;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static com.czh.staticmethod.StaticMethod.test;
 
 /**
@@ -8,5 +13,20 @@ import static com.czh.staticmethod.StaticMethod.test;
 public class Test {
     public static void main(String[] args) {
         test();
+        System.out.println("----------------");
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        map.put("char", list);
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        List<String> list2 = map.get("char");
+        for (String s : list2) {
+            System.out.println(s);
+        }
     }
 }
