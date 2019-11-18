f30e01b805e1d4c0817a8077627b27d951200a3e
czh <czhwin@outlook.com>
2019-11-18 11:49:38 +0800

    工厂类

---
+++ src/com/czh/publish/TesteAb.java
@@ -0,0 +1,10 @@
+package com.czh.publish;
+
+public abstract class TesteAb {
+    public String fuck = "fuck";
+    public void test() {
+        System.out.println("fuck");
+    }
+
+    public abstract  void test2();
+}
